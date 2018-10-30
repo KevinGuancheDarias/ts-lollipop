196a8e06e8f9ad3c97af65d15c2f9b731870e007
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2018-10-30 11:36:09 +0000

    Fix bad numbers in Package.json and add extra helper aliases

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -5,3 +5,6 @@ function lollipop-reinstall-cli () {
     npm uninstall -g @ts-lollipop/cli;
     npm install -g @ts-lollipop/cli;
 }
+
+alias lollipop-registry-npmjs='npm config set registry https://registry.npmjs.org/'
+alias lollipop-registry-verdaccio='npm config set registry http://127.0.0.1:4873';
